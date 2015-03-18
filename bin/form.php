2281fa6e01186fa2dc1b281d128e7950e1cf7b37
Audrey Bosquet <abosquet@mit.edu>
2015-03-18 04:38:01 -0400

    big gap in changes to php, js, and html form
    email not working again.

--- bin/form.php
+++ bin/form.php
@@ -1,25 +1,94 @@
 <?php
 // check if fields passed are empty
-if(empty($_POST['name'])  		||
-   empty($_POST['email']) 		||
-   empty($_POST['message'])	||
-   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+if(empty($_POST['first_name']) || empty($_POST['last_name'])){
+    echo "No arguments Provided!";
+    mail('audrey.bosquet@gmail.com', 'error', 'The name was empty, oops!', 'From: arianeanddustyn.com');
+	return false;
+}
+   
+if(empty($_POST['email'])){
+    echo "No arguments Provided!";
+    $name = $_POST['name'];
+    mail('audrey.bosquet@gmail.com', 'error', ' $name tried to submit your form, but the email was empty, oops!', 'From: arianeanddustyn.com');
+	return false;
+}
+if(empty($_POST['attend'])){
+    echo "No arguments Provided!";
+    $name = $_POST['name'];
+    mail('audrey.bosquet@gmail.com', 'error', ' $name tried to submit your form, but the attend was empty, oops!', 'From: arianeanddustyn.com');
+	return false;
+}
+if($_POST['email'] != 'none' && !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
 	echo "No arguments Provided!";
+    $name = $_POST['name'];
+    mail('audrey.bosquet@gmail.com', 'error', ' $name tried to submit your form, but the email was wrong, oops!', 'From: arianeanddustyn.com');
 	return false;
    }
+if(empty($_POST['guest_first_name'])|| empty($_POST['guest_last_name'])){
+    echo "No arguments Provided!";
+    $name = $_POST['name'];
+    mail('audrey.bosquet@gmail.com', 'error', ' $name tried to submit your form, but the guest name was empty, oops!', 'From: arianeanddustyn.com');
+	return false;
+}
+   
+if(empty($_POST['guest_email'])){
+    echo "No arguments Provided!";
+    $name = $_POST['name'];
+    mail('audrey.bosquet@gmail.com', 'error', ' $name tried to submit your form, but the guest email was empty, oops!', 'From: arianeanddustyn.com');
+	return false;
+}
+if(empty($_POST['guest_attend'])){
+    echo "No arguments Provided!";
+    $name = $_POST['name'];
+    mail('audrey.bosquet@gmail.com', 'error', ' $name tried to submit your form, but the guest attend was empty, oops!', 'From: arianeanddustyn.com');
+	return false;
+}
+if(empty($_POST['arrangements'])){
+    echo "No arguments Provided!";
+    $name = $_POST['name'];
+    mail('audrey.bosquet@gmail.com', 'error', ' $name tried to submit your form, but the arrangements was empty, oops!', 'From: arianeanddustyn.com');
+	return false;
+}
 	
-$name = $_POST['name'];
-$email_address = $_POST['email'];
+$first_name = $_POST['first_name'];
+$last_name = $_POST['last_name'];
+$email = $_POST['email'];
 $message = $_POST['message'];
-	
-// create email body and send it	
+$attend = $_POST['attend'];
+$guest_first_name = $_POST['guest_first_name'];
+$guest_last_name = $_POST['guest_last_name'];
+$guest_email_address = $_POST['guest_email'];
+$guest_attend = $_POST['guest_attend'];
+$arrangements = $_POST['arrangements'];
+
 $to = 'audrey.bosquet@gmail.com'; // put your email
-$email_subject = "Contact form submitted by:  $name";
-$email_body = "You have received a new message. \n\n".
-				  " Here are the details:\n \nName: $name \n ".
-				  "Email: $email_address\n Message \n $message";
-$headers = "From: arianeanddustyn.com\n";
+$email_subject = "Contact form submitted by: $first_name $last_name";
+
+$email_body = "$first_name $last_name has accepted your invitation! $first_name is also bringing a guest! \n".
+                    "guest information is not required but may have been submitted (will appear as normal or none) /n guest name: $guest_first_name $guest_last_name, guest email: $guest_email\n",
+                    "$first_name has selected the following arrangements: $arrangements\n",
+                    "Message as follows: $message";
+
+//if($attend == 'no'){
+//    $email_body = "$first_name $last_name has regretfully declined your invitation. \n".
+//                    "Message as follows: $message";
+//}
+//elseif($guest_attend == 'no'){
+//    $email_body = "$first_name $last_name has accepted your invitation! $first_name is not planning on bringing a guest \n".
+//                    "$first_name has selected the following arrangements: $arrangements\n",
+//                    "you may contact $first_name at $email.\n",
+//                    "Message as follows: $message";
+//}
+//else{
+//    $email_body = "$first_name $last_name has accepted your invitation! $first_name is also bringing a guest! \n".
+//                    "guest information is not required but may have been submitted (will appear as normal or none) /n guest name: $guest_first_name $guest_last_name, guest email: $guest_email\n",
+//                    "$first_name has selected the following arrangements: $arrangements\n",
+//                    "Message as follows: $message";
+//}
+
+// create email body and send it	
+$headers = "From: arianeanddustyn.com";
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
 return true;			
--- js/script.js
+++ js/script.js
@@ -3,8 +3,46 @@
    example is taken from jqBootstrapValidation docs 
   */
 $(function() {
+    
+    //Form hide/show areas.
+    $('input[name="attend"]').click(function() {
+        if($('#attend_yes').is(':checked')) { 
+            $('#email').attr('placeholder', 'Email');
+            $('#guest, input[name="email"]').fadeIn();
+            $('#rsvp_email').fadeIn();
+            $('#message').attr('placeholder', 'Message for the happy couple');
+            $('#rsvp_email').attr('placeholder', 'E-Mail');
+            $('#rsvp_guest_info input[type="text"]').attr('placeholder', 'Full Name');
+        }else{
+            console.log('This person is not attending.');
+            $('#guest, #rsvp_guest_info, input[name="email"]').fadeOut();
+            $('#rsvp_arrangements').fadeOut();
+            $('#rsvp_email').fadeOut();
+            $('#email').val('none');
+            $('#message').val('').attr('placeholder', 'Regrets');
+            $('#rsvp_message').fadeIn();
+            $('#guest_attend_no').prop('checked', true);
+            $('#arrangements_other').prop('checked', true);
+            $('#rsvp_guest_info input[type="text"], #rsvp_guest_info input[type="email"]').val('none');
+        }
 
- $("input,textarea").jqBootstrapValidation(
+        $('input[name="guest_attend"]').click(function() {
+            console.log("you made a selection about your guest!")
+            $('#rsvp_message').fadeIn(); //add #arrangements, 
+            $('#rsvp_arrangements').fadeIn();
+//            
+            if($('#guest_attend_yes').is(':checked')) { 
+                $('#rsvp_email').attr('placeholder', 'E-Mail');
+                $('#rsvp_guest_info input[type="text"]').attr('placeholder', 'Full Name');
+                $('#rsvp_guest_info').fadeIn();
+            }else{
+                $('#rsvp_guest_info').fadeOut();
+                $('#rsvp_guest_info input[type="text"], #rsvp_guest_info input[type="email"]').val('none');
+            }
+        });
+    });
+
+ $("input, select, textarea").jqBootstrapValidation(
     {
      preventSubmit: true,
      submitError: function($form, event, errors) {
@@ -13,19 +51,52 @@ $(function() {
      },
      submitSuccess: function($form, event) {
       event.preventDefault(); // prevent default submit behaviour
+         var attending = $("input[name='attend']:checked").val();
+         var val_err_msg = " ";
+         var proceed = true;
+            if (  ($("input[name='attend']").is(':checked'))  &&  (attending == 'yes') && (!$("input[name='guest_attend']").is(':checked')) ){
+                    val_err_msg += '<li>Please select if you are bringing a guest.</li>';
+                    $('.guest-error').append(val_err_msg);
+                    proceed = false; //set do not proceed flag
+            } 
+
+            if (!$("input[name='attend']").is(':checked')) {
+                val_err_msg += '<li>Please select whether or not you will be attending.</li>';
+                $('.attend-error').append(val_err_msg);
+                proceed = false; //set do not proceed flag
+            }
+            if (  ($("input[name='attend']").is(':checked'))  &&  (attending == 'yes') && ($("input[name='guest_attend']").is(':checked') && (!$("input[name='arrangements']").is(':checked'))) ){
+                    val_err_msg += '<li>Please select what arrangements you prefer.</li>';
+                    $('.arrangements-error').append(val_err_msg);
+                    proceed = false; //set do not proceed flag
+            } 
+         
        // get values from FORM
-       var name = $("input#name").val();  
+       var first_name = $("input#first_name").val();
+         var last_name = $("input#last_name").val();
        var email = $("input#email").val(); 
        var message = $("textarea#message").val();
-        var firstName = name; // For Success/Failure Message
-           // Check for white space in name for Success/Fail message
-        if (firstName.indexOf(' ') >= 0) {
-	   firstName = name.split(' ').slice(0, -1).join(' ');
-         }        
+         var attend = $("#contactForm input[name='attend']:checked").val();
+         var guest_first_name = $("input#guest_first_name").val(); 
+         var guest_last_name = $("input#guest_last_name").val(); 
+        var guest_email = $("input#guest_email").val(); 
+         var guest_attend = $("#contactForm input[name='guest_attend']:checked").val();
+         var arrangements = $("#contactForm input[name='arrangements']:checked").val();
+         
+//         if ($("input[name='guest_attend']").is(':checked') && (guest_first_name == '')){
+//                guest_first_name = 'none';
+//            }
+//         if ($("input[name='guest_attend']").is(':checked') && (guest_last_name == '')){
+//                guest_last_name = 'none';
+//            }
+        
+        console.log("Passing name: " + first_name + ", " + last_name + ", email: " + email + ", message: " + message + ", attend: " + attend + ", guest name: " + guest_first_name + ", " + guest_last_name + ", guest_email: " + guest_email + ", guest attend: " + guest_attend + ", arrangements: " + arrangements + " ...through ajax. Proceed = " + proceed);
+                  
+         if(proceed !== false){
 	 $.ajax({
                 url: "./bin/form.php",
             	type: "POST",
-            	data: {name: name, email: email, message: message},
+            	data: {first_name: first_name, last_name: last_name, email: email, message: message, attend: attend, guest_first_name: guest_first_name, guest_last_name: guest_last_name, guest_email: guest_email, guest_attend: guest_attend, arrangements: arrangements},
             	cache: false,
             	success: function() {  
             	// Success message
@@ -50,7 +121,7 @@ $(function() {
  		//clear all fields
  		$('#contactForm').trigger("reset");
  	    },
-           })
+           })}
          },
          filter: function() {
                    return $(this).is(":visible");
