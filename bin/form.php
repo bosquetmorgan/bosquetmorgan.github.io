20b3444edce78aa134de45bc4a60ec7c0286731a
abosquet <abosquet@mit.edu>
2015-03-22 15:06:10 -0400

    php working (on server) and starting registry

--- bin/form.php
+++ bin/form.php
@@ -1,33 +1,89 @@
 <?php
 // check if fields passed are empty
-if(empty($_POST['name'])  		||
-   empty($_POST['attend']) 		||
-   empty($_POST['email']) 		||
-   empty($_POST['message'])	||
-   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+if(empty($_POST['first_name']) || empty($_POST['last_name'])){
+    echo "No arguments Provided!";
+    mail('audrey.bosquet@gmail.com', 'error', 'The name was empty, oops!', 'From: arianeanddustyn.com');
+	return false;
+}
+
+$first_name = $_POST['first_name'];
+$last_name = $_POST['last_name'];
+$name = $first_name . ' ' . $last_name;
+
+if(empty($_POST['email'])){
+    echo "No arguments Provided!";
+    mail('audrey.bosquet@gmail.com', 'error', "$name tried to submit your form, but the email was empty, oops!", 'From: arianeanddustyn.com');
+	return false;
+}
+if(empty($_POST['attend'])){
+    echo "No arguments Provided!";
+    mail('audrey.bosquet@gmail.com', 'error', "$name tried to submit your form, but the attend was empty, oops!", 'From: arianeanddustyn.com');
+	return false;
+}
+if($_POST['email'] != 'none' && !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
 	echo "No arguments Provided!";
+    mail('audrey.bosquet@gmail.com', 'error', ' $name tried to submit your form, but the email was wrong, oops!', 'From: arianeanddustyn.com');
 	return false;
    }
-	
-$name = $_POST['name'];
-$email_address = $_POST['email'];
+if(empty($_POST['guest_name'])){
+    echo "No arguments Provided!";
+    mail('audrey.bosquet@gmail.com', 'error', "$name tried to submit your form, but the guest name was empty, oops!", 'From: arianeanddustyn.com');
+	return false;
+}
+   
+if(empty($_POST['guest_email'])){
+    echo "No arguments Provided!";
+    $name = $_POST['name'];
+    mail('audrey.bosquet@gmail.com', 'error', "$name tried to submit your form, but the guest email was empty, oops!", 'From: arianeanddustyn.com');
+	return false;
+}
+if(empty($_POST['guest_attend'])){
+    echo "No arguments Provided!";
+    $name = $_POST['name'];
+    mail('audrey.bosquet@gmail.com', 'error', "$name tried to submit your form, but the guest attend was empty, oops!", 'From: arianeanddustyn.com');
+	return false;
+}
+if(empty($_POST['arrangements'])){
+    echo "No arguments Provided!";
+    mail('audrey.bosquet@gmail.com', 'error', "$name tried to submit your form, but the arrangements was empty, oops!", 'From: arianeanddustyn.com');
+	return false;
+}
+
+$email = $_POST['email'];
 $message = $_POST['message'];
 $attend = $_POST['attend'];
-$attend_message = 'regretfully declines your wedding invitation.';
+$guest_name = $_POST['guest_name'];
+$guest_email = $_POST['guest_email'];
+$guest_attend = $_POST['guest_attend'];
+$arrangements = $_POST['arrangements'];
 
-if($attend == 'yes'){
-    $attend_message = 'has happily accepted your wedding invitation!';
+
+if($attend == 'no'){
+    $email_body = "$name regretfully declines your invitation. \n\n".
+        "message: \n $message";
 }
-	
-// create email body and send it	
-$to = 'audrey.bosquet@gmail.com'; // put your email
-$email_subject = "Contact form submitted by:  $name";
-$email_body = "You have received a new response for your wedding. \n\n".
-				  "$name $attend_message".
-				  "Email: $email_address\n Message \n $message";
-$headers = "From: arianeanddustyn.com\n";
-$headers .= "Reply-To: $email_address";	
+elseif($guest_attend == 'no'){
+    $email_body = "$name has accepted your invitation! \n\n".
+        "$first_name will not be bringing a guest \n".
+        "$first_name has indicated an lodging choice of: $arrangements \n".
+        "You can reach $first_name at $email.\n\n".
+        "Message: \n $message";
+}
+else{ 
+    $email_body = "$name has accepted your invitation! \n\n".
+        "$first_name is bringing a guest. Guest information that follows may or may not be complete: \n".
+        "Guest Name: $guest_name, Guest Email: $guest_email\n".
+        "$first_name has indicated an lodging choice of: $arrangements \n\n".
+        "You can reach $first_name at $email.\n\n".
+        "Message: \n $message";
+}
+
+$to = 'thehappycouple@arianeanddustyn.com'; // put your email
+$email_subject = "Contact form submitted by: $name";
+
+$headers = "From: $name ";
+$headers .= "Reply-To: RSVP ";	
 mail($to,$email_subject,$email_body,$headers);
 return true;			
 ?>
--- js/script.js
+++ js/script.js
@@ -2,12 +2,63 @@
   Jquery Validation using jqBootstrapValidation
    example is taken from jqBootstrapValidation docs 
   */
+function response(first, last, attend, email, guest_attend, guest_name, guest_email, arrangements, message) {
+    this.first_name = first;
+    this.last_name = last;
+    this.attend = attend;
+    this.email = email;
+    this.guest_attend = guest_attend;
+    this.guest_name = guest_name;
+    this.guest_email = guest_email;
+    this.arrangements = arrangements;
+    this.message = message;
+}
 
 $(function() {
-
-    //put show/hide here eventually
     
- $("input,textarea").jqBootstrapValidation(
+    //Form hide/show areas.
+    $('input[name="attend"]').click(function() {
+        
+        if($('#attend_yes').is(':checked')) { 
+            $('#email').attr('placeholder', 'E-mail');
+            $('#guest, input[name="email"]').fadeIn();
+            $('#rsvp_email').fadeIn();
+            $('#rsvp_message').fadeOut();
+            $('#message').attr('placeholder', 'Message for the happy couple');
+            $('#guest_email').attr('placeholder', 'E-Mail');
+            $('#guest_first_name').attr('placeholder', 'First Name');
+            $('#guest_last_name').attr('placeholder', 'Last Name');
+            $('#guest_attend').prop('checked', false);
+        }else{
+            console.log('This person is not attending.');
+            $('#guest, #rsvp_guest_info, input[name="email"]').fadeOut();
+            $('#rsvp_arrangements').fadeOut();
+            $('#rsvp_email').fadeOut();
+            $('#message').val('').attr('placeholder', 'Regrets');
+            $('#rsvp_message').fadeIn();
+            $('#guest_attend_no').prop('checked', true);
+            $('#arrangements_other').prop('checked', true);
+            $('#rsvp_guest_info input[type="text"]').val('none');
+        }
+
+        $('input[name="guest_attend"]').click(function() {
+            console.log("you made a selection about your guest!");
+            $('#rsvp_message').fadeIn(); //add #arrangements, 
+            $('#rsvp_arrangements').fadeIn();
+//            
+            if($('#guest_attend_yes').is(':checked')) { 
+                $('#guest_email').attr('placeholder', 'E-Mail');
+                $('#guest_first_name').attr('placeholder', 'First Name');
+                $('#guest_last_name').attr('placeholder', 'Last Name');
+                $('#rsvp_guest_info').fadeIn();
+            }else{
+                $('#rsvp_guest_info').fadeOut();
+                $('#rsvp_guest_info input[type="text"]').val('none');
+            }
+        });
+    });
+
+  $('form[name="sentMessage"]').find('input,select,textarea').not('[type=image]').jqBootstrapValidation(
     {
      preventSubmit: true,
      submitError: function($form, event, errors) {
@@ -16,20 +67,71 @@ $(function() {
      },
      submitSuccess: function($form, event) {
       event.preventDefault(); // prevent default submit behaviour
+         var attending = $("input[name='attend']:checked").val();
+         var val_err_msg = " ";
+         var proceed = true;
+            if (  ($("input[name='attend']").is(':checked'))  &&  (attending == 'yes') && (!$("input[name='guest_attend']").is(':checked')) ){
+                    val_err_msg += '<li>Please select if you are bringing a guest.</li>';
+                    $('.guest-error').append(val_err_msg);
+                    proceed = false; //set do not proceed flag
+            } 
+
+            if (!$("input[name='attend']").is(':checked')) {
+                val_err_msg += '<li>Please select whether or not you will be attending.</li>';
+                $('.attend-error').append(val_err_msg);
+                proceed = false; //set do not proceed flag
+            }
+            if (  ($("input[name='attend']").is(':checked'))  &&  (attending == 'yes') && ($("input[name='guest_attend']").is(':checked') && (!$("input[name='arrangements']").is(':checked'))) ){
+                    val_err_msg += '<li>Please select what arrangements you prefer.</li>';
+                    $('.arrangements-error').append(val_err_msg);
+                    proceed = false; //set do not proceed flag
+            } 
+         
        // get values from FORM
-       var name = $("input#name").val();  
-       var email = $("input#email").val(); 
-       var message = $("textarea#message").val();
-         var attend = $("#contactForm input[name='attend']:checked").val();
-        var firstName = name; // For Success/Failure Message
-           // Check for white space in name for Success/Fail message
-        if (firstName.indexOf(' ') >= 0) {
-	   firstName = name.split(' ').slice(0, -1).join(' ');
-         }        
+        var first_name = $("input#first_name").val();
+        var last_name = $("input#last_name").val();
+        var email = $("input#email").val(); 
+        var message = $("textarea#message").val();
+        var attend = $("#contactForm input[name='attend']:checked").val();
+        var guest_first_name = $("input#guest_first_name").val(); 
+        var guest_last_name = $("input#guest_last_name").val(); 
+        var guest_email = $("input#guest_email").val(); 
+        var guest_attend = $("#contactForm input[name='guest_attend']:checked").val();
+        var arrangements = $("#contactForm input[name='arrangements']:checked").val();
+        var name = first_name + ' ' + last_name;
+         
+         if ($("input[name='guest_attend']").is(':checked') && (guest_first_name == '')){
+                guest_first_name = 'none';
+            }
+         if ($("input[name='guest_attend']").is(':checked') && (guest_last_name == '')){
+                guest_last_name = 'none';
+            }
+         if ($("input[name='guest_attend']").is(':checked') && (guest_email == '')){
+                guest_email = 'none';
+            }
+         if (message == ''){
+                message = 'none';
+            }
+         if (email == ''){
+                email = 'none';
+            }
+         if (guest_email == ''){
+                guest_email = 'none';
+            }
+        var name = first_name + last_name;
+        var guest_name = guest_first_name + ' ' + guest_last_name;
+        
+        console.log("Passing name: " + first_name + ", " + last_name + ", email: " + email + ", message: " + message + ", attend: " + attend + ", guest name: " + guest_first_name + ", " + guest_last_name + ", guest_email: " + guest_email + ", guest attend: " + guest_attend + ", arrangements: " + arrangements + " ...through ajax. Proceed = ");
+         
+         //add object to responses array
+//         responses.window[name] = new response(first_name, last_name, attend, email, guest_attend, guest_name, guest_email, arrangements, message);
+         
+         
+         if(proceed !== false){
 	 $.ajax({
                 url: "./bin/form.php",
             	type: "POST",
-            	data: {name: name, email: email, message: message},
+            	data: {first_name: first_name, last_name: last_name, email: email, message: message, attend: attend, guest_name: guest_name, guest_email: guest_email, guest_attend: guest_attend, arrangements: arrangements},
             	cache: false,
             	success: function() {  
             	// Success message
@@ -49,12 +151,13 @@ $(function() {
  		 $('#success').html("<div class='alert alert-danger'>");
             	$('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             	 .append( "</button>");
-            	$('#success > .alert-danger').append("<strong>Sorry "+firstName+" it seems that my mail server is not responding...</strong> Could you please email me directly to <a href='mailto:audrey.bosquet@gmail.com?Subject=Message_Me from arianeanddustyn.com'>audrey.bosquet@gmail.com</a> ? Sorry for the inconvenience!");
+            	$('#success > .alert-danger').append("<strong>Sorry "+first_name+" it seems that my mail server is not responding...</strong> Could you please email me directly to <a href='mailto:audrey.bosquet@gmail.com?Subject=Message_Me from arianeanddustyn.com'>audrey.bosquet@gmail.com</a> ? Sorry for the inconvenience!");
  	        $('#success > .alert-danger').append('</div>');
  		//clear all fields
  		$('#contactForm').trigger("reset");
  	    },
-           })
+           });
+     }
          },
          filter: function() {
                    return $(this).is(":visible");
